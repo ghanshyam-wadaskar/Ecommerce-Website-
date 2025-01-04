12880d20546ed0f6f5b68f47146b2cf89047066f
Ghanshyam Siddhanath Wadaskar <147121410+ghanshyam-wadaskar@users.noreply.github.com>
2025-01-04 21:20:15 +0530

    Add files via upload

--- README.md
+++
@@ -1,1 +0,0 @@
-# codeant-frontend
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#426",
+        secondary: "#63e6",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
